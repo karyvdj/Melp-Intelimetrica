6351cd632fb750e785aa1c1731425ef88ee945ce
karyvdj <kaia51@hotmail.com>
2017-08-30 15:31:21 -0500

    Mostrar los nombres en consola

--- js/app.js
+++ js/app.js
@@ -2,13 +2,42 @@ var loadPage = function(){
   loadRestaurant();
 
 }
+var array = [];
 
 var loadRestaurant = function () {
   var url = "https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json";
-  $.getJSON(url,function (response) {
-    var restaurant = response.results;
-
-  });
+  $.getJSON(url, function (response) {
+    response.forEach(function (data) {
+      var name = data.name;
+      console.log(name)
+      array.push(name)
+      array.sort();
+      showRestaurant(name)
+    })
+  })
 };
 
+
+
+
 $(document).ready(loadPage);
+
+
+/*var request = new XMLHttpRequest();
+//console.log(request)
+request.open("GET", url);
+request.onload =function() {
+  var info = JSON.parse(request.responseText);
+  info.forEach(function (response) {
+    var name = response.name;
+    console.log(name)
+    array.push(name)
+    array.sort();
+  });
+}
+  array.forEach(function(name){
+    var arrayName = [];
+    arrayName.push(name);
+  //showRestaurant(arrayName);
+});
+request.send();*/
